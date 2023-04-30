90c51acb53890164c082681808c1f9e811329c9d
Alexander Dang <alexdang1993374@gmail.com>
2023-05-01 01:34:42 +0700

    fix: simpsons

---
+++ client/src/components/Simpsons/img/index.js
@@ -0,0 +1,38 @@
+import zaus from "./zaus.jpg";
+
+import mole from "./mole.jpg";
+
+import haha from "./haha.gif";
+
+import duff from "./duff.gif";
+
+import hippo from "./hippo.gif";
+
+import dead from "./dead.jpg";
+
+import flanders from "./flanders.gif";
+
+import hell from "./hell.gif";
+
+import rake from "./rake.gif";
+
+import blurst from "./blurst.gif";
+
+import damn from "./damn.jpg";
+
+import carumba from "./carumba.jpg";
+
+export {
+  zaus,
+  mole,
+  haha,
+  duff,
+  hippo,
+  dead,
+  flanders,
+  hell,
+  rake,
+  blurst,
+  damn,
+  carumba,
+};
---
+++ client/src/components/Simpsons/simpsonsCards.js
@@ -0,0 +1,77 @@
+import {
+  zaus,
+  mole,
+  haha,
+  duff,
+  hippo,
+  dead,
+  flanders,
+  hell,
+  rake,
+  blurst,
+  damn,
+  carumba,
+} from "./img";
+
+export const simpsonsCards = [
+  {
+    id: "zaus",
+    image: zaus,
+    title: "Dr. Zaius",
+  },
+  {
+    id: "mole",
+    image: mole,
+    title: "Gay for Moleman",
+  },
+  {
+    id: "haha",
+    image: haha,
+    title: "Haha",
+  },
+  {
+    id: "duff",
+    image: duff,
+    title: "Oh Yeah!!",
+  },
+  {
+    id: "hippo",
+    image: hippo,
+    title: "Mmmmm",
+  },
+  {
+    id: "dead",
+    image: dead,
+    title: "Stop he's already dead",
+  },
+  {
+    id: "flanders",
+    image: flanders,
+    title: "Stupid sexy Flanders",
+  },
+  {
+    id: "hell",
+    image: hell,
+    title: "What the hell",
+  },
+  {
+    id: "rake",
+    image: rake,
+    title: "Urghhh",
+  },
+  {
+    id: "blurst",
+    image: blurst,
+    title: "The greatest novel known to man",
+  },
+  {
+    id: "damn",
+    image: damn,
+    title: "Vegetables",
+  },
+  {
+    id: "carumba",
+    image: carumba,
+    title: "¡Ay, caramba!",
+  },
+];
